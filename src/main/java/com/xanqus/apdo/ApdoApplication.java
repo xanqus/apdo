ac5ef96fcc608a2aa05655f19ae56d8ed6e17137
xanqus <sbk98031816@gmail.com>
2021-12-13 17:47:14 +0900

    setting

---
+++ src/main/java/com/xanqus/apdo/ApdoApplication.java
@@ -0,0 +1,13 @@
+package com.xanqus.apdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xanqus/apdo/ApdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanqus.apdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
